554418601b90c98560a2daeca207d118cbe8a541
GaluL <gal.klm@gmail.com>
2016-09-10 10:50:57 +0300

    added SPCommonDefs.h

---
+++ SPCommonDefs.h
@@ -0,0 +1,18 @@
+/*
+ * SPCommonDefs.h
+ *
+ *  Created on: Sep 9, 2016
+ *      Author: galkl
+ */
+
+#ifndef SPCOMMONDEFS_H_
+#define SPCOMMONDEFS_H_
+
+// Error Messages
+#define SP_ALLOCATION_FAILURE "Allocation failure"
+#define SP_BAD_ARGUMENT "Bad argument was passed"
+
+#define SP_INVALID_NEG_VALUE -1
+
+
+#endif /* SPCOMMONDEFS_H_ */
